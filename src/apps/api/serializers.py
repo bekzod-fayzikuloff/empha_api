0c491144851e0bbb4e4e29112245c8b9444500ae
fayzikuloff.b <fayzikuloff.b@gmail.com>
2023-03-14 17:09:56 +0300

    :fire: Remove password confirmation

--- src/apps/api/serializers.py
+++ src/apps/api/serializers.py
@@ -1,7 +1,4 @@
-from typing import Optional, OrderedDict
-
 from django.contrib.auth.models import User
-from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
@@ -10,21 +7,13 @@ class RegisterSerializer(serializers.ModelSerializer):
     """User register serializer class"""
 
     email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
-
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password_confirm = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         """Metaclass with Serializer getting model and included fields"""
 
         model = User
-        fields = ("username", "password", "password_confirm", "email")
-
-    def validate(self, attrs: OrderedDict) -> Optional[OrderedDict]:
-        """Validate password."""
-        if attrs["password"] != attrs["password_confirm"]:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
+        fields = ("username", "password", "email")
 
     def create(self, validated_data: dict) -> User:
         """Creating a new user instance."""
